67e0eeba7d2e6baf7afad21b19184d87909f7b6c
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2022-07-15 17:21:27 +0600

    added my skills logo

--- src/Component/Skills/Skills.js
+++ src/Component/Skills/Skills.js
@@ -1,58 +1,50 @@
 import React from 'react';
 import 'boxicons';
-
-
+import html from '../../assest/Icons/html.svg'
+import css from '../../assest/Icons/css-3-logo-png-transparent.png'
+import js from '../../assest/Icons/javascript.png'
+import reactI from '../../assest/Icons/React-icon.svg.png'
+import bootstrap from '../../assest/Icons/bootstrap.png'
+import tailwindcss from '../../assest/Icons/tailwind.png'
+// import node from '../../assest/Icons/node-js-2.png'
+// import express from '../../assest/Icons/Expressjs.png'
+// import mongoDB from '../../assest/Icons/mongodb.png'
+import mern from '../../assest/Icons/MERN-logo.png'
+import wordpress from '../../assest/Icons/wordpress_PNG74.png'
+import './skills.styles.css'
 
 const Skills = () => {
     return (
         <div className='text-neutral mt-14'>
-            <h1 class=" text-2xl font-bold  text-center sm:text-4xl lg:text-3xl xl:text-4xl">MY SKILLS</h1>
+            <h1 className="text-accent text-center text-4xl font-bold my-7 ">My Skills</h1>
+
             <div className="flex    justify-center">
 
-                <div className="w-1/2">
+                <div className="w-">
 
                     <div>
-                        <h2 className="text-center"> Front-End</h2>
-                        <div className="flex gap-1 justify-center flex-wrap">
 
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
+                        <div className="flex gap-3 justify-center flex-wrap">
+
+                            <div className='imgDiv'><img src={html} alt='React' /></div>
+                            <div className='imgDiv'><img src={css} alt='React' /></div>
+                            <div className='imgDiv'><img src={bootstrap} alt='React' /></div>
+                            <div className='imgDiv'><img src={tailwindcss} alt='React' /></div>
+                            <div className='imgDiv'><img src={js} alt='React' /></div>
+                            <div className='imgDiv'><img src={reactI} alt='React' /></div>
+                            {/* <div className='imgDiv'><img src={node} alt='React' /></div>
+                            <div className='imgDiv'><img src={express} alt='React' /></div>
+                            <div className='imgDiv'><img src={mongoDB} alt='React' /></div> */}
+                            <div className='imgDiv'><img src={mern} alt='React' /></div>
+                            <div className='imgDiv'><img src={wordpress} alt='React' /></div>
                         </div>
                     </div>
-                    <div>
-                        <h2 className="text-center"> Back-End</h2>
-                        <div className="flex gap-1 justify-center flex-wrap">
 
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                            <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                        </div>
-                    </div>
 
                 </div>
 
 
-                <div className='w-1/2'>
-                    <h2 className="text-center"> </h2>
 
-
-                    <div className="flex gap-1 justify-center flex-wrap">
-                        <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                        <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                        <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-                        <div  ><img className='w-12' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' alt='React' /></div>
-
-                    </div>
-                </div>
 
             </div>
         </div>
